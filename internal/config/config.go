af791be47de302b61888128fce5d2766f9bf88f7
assilbekov <askhat.assilbekov@gmail.com>
2024-04-11 06:40:16 -0300

    add fetchConfigPath

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"os"
+	"time"
+)
 
 type Config struct {
 	Env         string        `yaml:"env" env-default:"local"`
@@ -12,4 +16,21 @@ type Config struct {
 type GRPCConfig struct {
 	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
+}
+
+// fetchConfigPath fetches config path from command line arguments or environment variables.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	// --config="/path/to/config.yaml"
+	flag.StringVar(&res, "config", "", "config path")
+	flag.Parse()
+
+	if res != "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
 }
