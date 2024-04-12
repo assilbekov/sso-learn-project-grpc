007bb5622c06adb8793705d29b07e8eb0debc5fd
assilbekov <askhat.assilbekov@gmail.com>
2024-04-12 10:56:42 -0300

    add server api implementation fake

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,14 @@
+package auth
+
+import (
+	ssov1 "github.com/assilbekov/grpc-app-course-protoes/gen/go/sso"
+	"google.golang.org/grpc"
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+}
+
+func Register(gRPC *grpc.Server) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+}
