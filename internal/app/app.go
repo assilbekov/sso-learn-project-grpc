afb8c69c6c7f3afa6b4d57c4f84b875486e654ca
assilbekov <askhat.assilbekov@gmail.com>
2024-04-12 11:05:35 -0300

    add app package

---
+++ internal/app/app.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"google.golang.org/grpc"
+	"log/slog"
+	authgrpc "sso-learn-project-grpc/internal/grpc/auth"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func New(log *slog.Logger, port int) *App {
+	gRPCServer := grpc.NewServer()
+
+	authgrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
