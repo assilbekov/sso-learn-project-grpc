a6435c65a476d2e13cea98caa190829d3e880bf9
assilbekov <askhat.assilbekov@gmail.com>
2024-04-13 04:38:19 -0300

    add login validation

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	ssov1 "github.com/assilbekov/grpc-app-course-protoes/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type serverAPI struct {
@@ -14,10 +16,26 @@ func Register(gRPC *grpc.Server) {
 	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
 }
 
+const (
+	emptyValue = 0
+)
+
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
 	return &ssov1.LoginResponse{
 		Token: req.GetEmail(),
 	}, nil
