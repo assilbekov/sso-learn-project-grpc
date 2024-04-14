8efb5f48df268f25a0fed5696aa42e2bcd62a848
assilbekov <askhat.assilbekov@gmail.com>
2024-04-14 06:16:53 -0300

    add auth new func

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"log/slog"
 	"sso-learn-project-grpc/internal/domain/models"
+	"time"
 )
 
 type Auth struct {
-	log *slog.Logger
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
 }
 
 type UserSaver interface {
@@ -21,4 +26,24 @@ type UserSaver interface {
 type UserProvider interface {
 	User(ctx context.Context, email string) (models.User, error)
 	isAdmin(ctx context.Context, userID string) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int64) (models.App, error)
+}
+
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:         log,
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
 }
