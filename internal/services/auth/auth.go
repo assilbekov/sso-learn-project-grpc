49bcd2e251dbca868216c33e0d1be0b59d18ae51
assilbekov <askhat.assilbekov@gmail.com>
2024-04-14 06:11:01 -0300

    add auth interfaces

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,1 +1,24 @@
 package auth
+
+import (
+	"context"
+	"log/slog"
+	"sso-learn-project-grpc/internal/domain/models"
+)
+
+type Auth struct {
+	log *slog.Logger
+}
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	isAdmin(ctx context.Context, userID string) (bool, error)
+}
